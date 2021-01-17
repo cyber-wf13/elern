856726a537dac506a938ad76b9155ec70c729417
cyber-wf13 <o.zon77788@gmail.com>
2021-01-17 07:51:17 +0200

    initial commit

---
+++ js/js-jquery.js
@@ -0,0 +1,5 @@
+// let $menuBoxLogo = $('.menu-box__logo');
+// $menuBoxLogo.css.width='1000px';
+// console.log($menuBoxLogo.css('width','1000px'));
+// console.log($menuBoxLogo);
+// console.log($('.menu-box__logo').css('height'));
